4334b24888cbbee522a1328d4089d1c0c4c6a1ac
toheedakhtar <toheedak333@gmail.com>
2024-05-16 19:48:42 +0530

    finishing up

--- app.py
+++ app.py
@@ -5,6 +5,32 @@ import torch
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
 
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+
+model_id = "openai/whisper-tiny"
+
+model = AutoModelForSpeechSeq2Seq.from_pretrained(
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+)
+model.to(device)
+
+processor = AutoProcessor.from_pretrained(model_id)
+
+pipe = pipeline(
+    "automatic-speech-recognition",
+    model=model,
+    tokenizer=processor.tokenizer,
+    feature_extractor=processor.feature_extractor,
+    max_new_tokens=128,
+    chunk_length_s=15,
+    batch_size=16,
+    return_timestamps=True,
+    torch_dtype=torch_dtype,
+    device=device,
+)
+
+
 def transcribe(url):
     # get video and extract video
     def get_video(yt_url):
@@ -33,31 +59,7 @@ def transcribe(url):
 
 
     # transcribe audio
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-
-    model_id = "openai/whisper-tiny"
-
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
-    )
-    model.to(device)
-
-    processor = AutoProcessor.from_pretrained(model_id)
-
-    pipe = pipeline(
-        "automatic-speech-recognition",
-        model=model,
-        tokenizer=processor.tokenizer,
-        feature_extractor=processor.feature_extractor,
-        max_new_tokens=128,
-        chunk_length_s=15,
-        batch_size=16,
-        return_timestamps=True,
-        torch_dtype=torch_dtype,
-        device=device,
-    )
-
+   
     audio = 'audio.wav'
 
     text_audio = pipe(audio)
@@ -98,4 +100,10 @@ def transcribe(url):
     
     return sample_output_list
 
-print(transcribe("https://www.youtube.com/watch?v=CeSAjK2CBEA&t=77s"))
+intf = gr.Interface(
+    fn=transcribe,
+    inputs = ["text"],
+    outputs = ["text"]
+)
+
+intf.launch()
